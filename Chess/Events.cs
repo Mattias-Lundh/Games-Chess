afe7861af57e3e5022db276d814f6287093f5814
Mattias Lundh <mattiasanderslundh@gmail.com>
2017-10-29 10:36:06 +0100

    improved highlights

--- Chess/Events.cs
+++ Chess/Events.cs
@@ -11,32 +11,49 @@ namespace Chess
     class Events
     {
         public static void PieceMouseDownEvent(object sender, MouseEventArgs e)
-        {            
-            if (Game.SelectedPiece != null) { UnHighlightSquares(); }
+        {
+            if (Game.SelectedPiece != null && Game.SelectedPiece.Player == Game.Player) { UnHighlightSquares(); }
             PictureBox picturebox = (PictureBox)sender;
             Game.SelectedPiece = ChessPiece.Find[picturebox];
-            HighlightSquares();
+            if (Game.Player == Game.SelectedPiece.Player)
+            {
+                FollowMouse(Game.SelectedPiece);
+                HighlightSquares();
+            }
         }
-
+        
         public static void PieceClickEvent(object sender, EventArgs e)
         {
-            if (Game.SelectedPiece != null) { UnHighlightSquares(); }
+            if (Game.SelectedPiece != null && Game.SelectedPiece.Player == Game.Player) { UnHighlightSquares(); }
             PictureBox picturebox = (PictureBox)sender;
             Game.SelectedPiece = ChessPiece.Find[picturebox];
-            HighlightSquares();
-        }
+            if (Game.Player == Game.SelectedPiece.Player)
+            {
+                HighlightSquares();
+            }
+        }        
 
         public static void SquareMouseUpEvent(object sender, MouseEventArgs e)
         {
-           
+
+        }
+
+        private static void FollowMouse(ChessPiece piece)
+        {
+            piece.Graphic.Visible = false;
+            PictureBox graphic = new PictureBox
+            {
+                ImageLocation = piece.Graphic.ImageLocation,
+            };
         }
 
         private static void HighlightSquares()
         {
             foreach (string address in Game.HighLightList)
             {
-                Board.Square[address].Panel.BackColor = Color.MediumBlue;
+                Board.Square[address].Panel.BackColor = Color.GreenYellow;
             }
+            Board.Square[Game.SelectedPiece.Address].Panel.BackColor = Color.ForestGreen;
 
         }
         private static void UnHighlightSquares()
@@ -45,6 +62,7 @@ namespace Chess
             {
                 Board.Square[address].Panel.BackColor = Board.Square[address].FillColor;
             }
+            Board.Square[Game.SelectedPiece.Address].Panel.BackColor = Board.Square[Game.SelectedPiece.Address].FillColor;
         }
     }
 }
